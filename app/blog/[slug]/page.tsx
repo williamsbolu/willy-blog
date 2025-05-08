32ec5a640c1e33f80c27bf17f5293ed15b1b944e
williams bolu <williams.bolu99@gmail.com>
2025-05-08 15:06:58 +0100

    added cache revalidation

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -3,6 +3,8 @@ import { fullBlog } from "@/lib/interface";
 import { client, urlFor } from "@/lib/sanity";
 import { PortableText } from "@portabletext/react";
 
+export const revalidate = 30;
+
 async function getData(slug: string) {
   const query = `
         *[_type == "blog" && slug.current == '${slug}'] {
